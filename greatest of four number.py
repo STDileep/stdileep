bb4330192a887e5b09ed2286c4f09d2c5666080b
STDileep <145823814+STDileep@users.noreply.github.com>
2023-09-23 10:16:25 +0530

    Add files via upload

---
+++ greatest of four number.py
@@ -0,0 +1,12 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=int(input("enter c value:"))
+d=int(input("enter d value:"))
+if(a>b,a>c,a>d):
+    print("the value of a is greater")
+elif(b>a,b>c,b>d):
+    print("the value of b is greater")
+elif(c>a,c>b,c>d):
+    print("the value f c is gerater")
+else:
+    print("the value of d is greater")
